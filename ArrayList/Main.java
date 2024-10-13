b177169c80107c80890a401d607afe79ef152b3d
khaiky <samanthagabitano015@gmail.com>
2024-10-13 21:06:57 +0800

    added scanner to have user inputs

--- ArrayList/Main.java
+++ ArrayList/Main.java
@@ -2,5 +2,9 @@
 package ArrayList;
 import java.util.Scanner;
 public class Main {
-    
+     public static void main(String[] args){
+        
+        //Added a scanner to have user inputs
+        Scanner sc = new Scanner(System.in);
+}
 }
