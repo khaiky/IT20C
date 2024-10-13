b09bdd02550cee458ad9a6b8612babb59cb4e45d
khaiky <samanthagabitano015@gmail.com>
2024-10-13 19:20:00 +0800

    added stack int size

--- topic_3_stack/Stack.java
+++ topic_3_stack/Stack.java
@@ -5,4 +5,11 @@ public class Stack {
     private int maxSize;
     private int[] stackArray;
     private int top;
+    
+     // Constructor should be declared without a return type (void)
+    public Stack(int size) {
+        this.maxSize = size;
+        this.stackArray = new int[maxSize];
+        this.top = -1;
+}
 }
