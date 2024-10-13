7d77ce38b2b7bcd648ea6921390e70ece224e60d
khaiky <samanthagabitano015@gmail.com>
2024-10-13 21:34:45 +0800

    creating an object to access the linkedlist class

---
+++ Topic_2_linkedlist_string/Main.java
@@ -0,0 +1,17 @@
+
+package Topic_2_linkedlist_string;
+import java.util.Scanner;
+
+public class Main {
+      public static void main(String[] args) {
+		
+	//Creating an object to access the LinkedList Class
+        LinkedList list = new LinkedList();
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Hello, Welcome!");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~");
+            
+}
+}
