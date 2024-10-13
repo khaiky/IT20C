405a862f413e6f7d0d5b6d5a2ad79b85d4f2dc18
khaiky <samanthagabitano015@gmail.com>
2024-10-13 21:39:05 +0800

    added method to add a new node at the of the list

--- Topic_2_linkedlist_string/LinkedList.java
+++ Topic_2_linkedlist_string/LinkedList.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Topic_2_linkedlist_string;
 
-/**
- *
- * @author Admin
- */
+
 public class LinkedList {
-    
+     private Node head; 
+
+    // Method to add a new node at the end of the list
+    public void add(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+}
 }
