c47a10d8667ba461e0df0dc11e6218709db183e3
khaiky <samanthagabitano015@gmail.com>
2024-10-13 22:30:06 +0800

    creating object to access the linked list class

--- Topic_2_linkedlist_int/Main.java
+++ Topic_2_linkedlist_int/Main.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Topic_2_linkedlist_int;
 
-/**
- *
- * @author Admin
- */
+import java.util.Scanner;
 public class Main {
-    
+     public static void main(String[] args) {
+        
+        //Creating an object to access the LinkedList Class
+        Linkedlist list = new Linkedlist();
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Hello there! Welcome!!");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~");
+}
 }
