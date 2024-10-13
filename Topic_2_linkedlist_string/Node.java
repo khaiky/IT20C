a0fbd33ac36809627b9357b91792a15de0df4843
khaiky <samanthagabitano015@gmail.com>
2024-10-13 21:41:26 +0800

    added node as the constructor

--- Topic_2_linkedlist_string/Node.java
+++ Topic_2_linkedlist_string/Node.java
@@ -1,14 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Topic_2_linkedlist_string;
 
-/**
- *
- * @author Admin
- */
+
 public class Node {
-    
+    String data;  
+    Node next;    
+
+    // Constructor
+    Node(String data) {
+        this.data = data;
+        this.next = null;
+    }
 }
+
