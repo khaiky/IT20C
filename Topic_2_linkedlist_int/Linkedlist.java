84a5f09eacac39f459c67e1db026e2cf504b1905
khaiky <samanthagabitano015@gmail.com>
2024-10-13 22:21:09 +0800

    added method to print the linked list

--- Topic_2_linkedlist_int/Linkedlist.java
+++ Topic_2_linkedlist_int/Linkedlist.java
@@ -11,6 +11,23 @@ public class Linkedlist {
             if (head == null) {
                 head = newNode;
                 return;
+                
+                  }
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
             }
-}
-}
+            current.next = newNode;
+        }
+          // Method to print the linked list
+        public void printList() {
+            Node current = head;
+            while (current != null) {
+                System.out.print(current.data + " -> ");
+                current = current.next;
+            }
+            System.out.println("null");
+        }
+            }
+
+
