a1a285e91ea19e4c6be5cd9ad9755d3771541550
khaiky <samanthagabitano015@gmail.com>
2024-10-13 20:04:45 +0800

    Added array

---
+++ DisplayViaForLoop/DisplayViaForLoop.java
@@ -0,0 +1,13 @@
+
+package DisplayViaForLoop;
+
+
+public class DisplayViaForLoop {
+    
+     //Arrays 
+        String[] names = {"khai", "Zion", "Lorcan", "Calyx"};
+        String[] place = {"New Zealand", "Switzerland", "Amsterdam", "Europe"};
+        String[] favoriteSongName = {"Campfire", "Sagip", "Blue", "The most beautiful thing"};
+        
+        
+}
