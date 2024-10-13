91b00ef27eb02585f4671616e93b386b57f6e85e
khaiky <samanthagabitano015@gmail.com>
2024-10-13 19:03:45 +0800

    adding import scanner and stack

---
+++ topic_3_stack/Main.java
@@ -0,0 +1,14 @@
+
+package topic_3_stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args){
+     Scanner sc = new Scanner(System.in);
+	 Stack  stack = new Stack();
+	
+         System.out.println("Hello, Welcome!");
+}
+}
