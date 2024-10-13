34bc62493791dd663b5a208786a6b045d42a68b6
khaiky <samanthagabitano015@gmail.com>
2024-10-13 20:56:03 +0800

    added some changes

--- ArrayList/ItemManager.java
+++ ArrayList/ItemManager.java
@@ -1,7 +1,21 @@
 //Adding package for arraylist
 package ArrayList;
 
-import java.util.Scanner;
+import java.util.ArrayList;
 public class ItemManager {
-    
+    private ArrayList<String> items;
+        
+        public ItemManager(){
+            items = new ArrayList();
+             //Added function to display the items
+        }
+        public void displayItems(){
+            System.out.println("Names on our list: " +items);
+        }
+        //Added a function to add items into the arraylist
+        public void addName(String item){
+            items.add(item);
+            System.out.println("Names: " + items);
+        }
 }
+
