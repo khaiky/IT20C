5b914482479e3f3b67df2dc6c2fedd368f78f0c0
khaiky <samanthagabitano015@gmail.com>
2024-10-13 21:37:51 +0800

    Added case 1 that display elements to the list

--- Topic_2_linkedlist_string/Main.java
+++ Topic_2_linkedlist_string/Main.java
@@ -20,7 +20,30 @@ public class Main {
                 + " 2. Delete an Item"
                 + " 3. Move/Swap an Item"
                 + " 4. Exit");
+             int choice = sc.nextInt();
             
+           switch(choice) {
+                case 1:
+                    // Adding elements to the list
+                        System.out.println("Add 5 Elements: ");
+                        sc.nextLine();
+                        String item1 = sc.nextLine();
+                        String item2 = sc.nextLine();
+                        String item3 = sc.nextLine();
+                        String item4 = sc.nextLine();
+                        String item5 = sc.nextLine();
+
+                        list.add(item1);
+                        list.add(item2);
+                        list.add(item3);
+                        list.add(item4);
+                        list.add(item5);
+
+                        System.out.println("------------------------------");
+                        list.printList();
+                        System.out.println("------------------------------");
+                        break;
+}
 }
 }
 }
