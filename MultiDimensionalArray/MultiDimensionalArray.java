8ac18974b429a6c1b422423a56fcf2e49bad4b4f
khaiky <samanthagabitano015@gmail.com>
2024-10-13 20:24:26 +0800

    added arrays

---
+++ MultiDimensionalArray/MultiDimensionalArray.java
@@ -0,0 +1,15 @@
+
+package MultiDimensionalArray;
+
+
+public class MultiDimensionalArray {
+    public static void main(String[] args){
+         String[][] arrays = {
+            {".", "-", "-", "-", "."},
+            {"/", "0", ",", "0", "/", ",", " ", "?"},
+            {"[", " ", "-", " ", "]"},
+            {"/", "-", "/", "-", "/"},
+            
+        };
+    }
+}
