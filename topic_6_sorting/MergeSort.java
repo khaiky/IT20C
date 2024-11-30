99b8a9d492388972ed9725040de358591652cf41
khaiky <samanthagabitano015@gmail.com>
2024-11-30 14:46:03 +0800

    added Case 3 for MergeSort

--- topic_6_sorting/MergeSort.java
+++ topic_6_sorting/MergeSort.java
@@ -93,7 +93,12 @@ public class MergeSort {
                             printArray(arr);
                             System.out.println("Sorting completed in: " + timeTaken + " milliseconds");
                         
-                            break;   
+                            break; 
+                            case 3:
+                            System.out.println();
+                            System.out.println("Thank you! Comeback and run it again.");
+                            System.exit(0);
+                            break;    
 }
     }
 }
