b83374f619b03bbac244b753ce68dbea129a507c
khaiky <samanthagabitano015@gmail.com>
2024-11-30 14:57:51 +0800

    added Case 2 for QuickSort

--- topic_6_sorting/QuickSort.java
+++ topic_6_sorting/QuickSort.java
@@ -65,6 +65,28 @@ public class QuickSort {
                               arr[i] = sc.nextInt();
                             }
                             break;
+                            
+                             case 2: 
+                            System.out.println();
+                            if (arr == null) {
+                                System.out.println("Elements is not yet added. Please add elements first.");
+                                break;
+                            }
+                            
+                            System.out.println("Original array:");
+                            printArray(arr);
+
+                            long startTime = System.currentTimeMillis();
+                            quickSort(arr, 0, arr.length - 1); 
+                            
+                            long endTime = System.currentTimeMillis();
+                            timeTaken = startTime - endTime;
+                            
+                            System.out.println("Sorted array:");
+                            printArray(arr);
+                            System.out.println("Sorting completed in: " + timeTaken + " milliseconds");
+                        
+                            break; 
                 
 }
       }
