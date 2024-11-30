902ffd35070cec86c9eff07d92af449ac2843679
khaiky <samanthagabitano015@gmail.com>
2024-11-30 14:47:39 +0800

    added default option

--- topic_6_sorting/MergeSort.java
+++ topic_6_sorting/MergeSort.java
@@ -98,7 +98,10 @@ public class MergeSort {
                             System.out.println();
                             System.out.println("Thank you! Comeback and run it again.");
                             System.exit(0);
-                            break;    
+                            break;  
+                            
+                              default:
+                            System.out.println("Invalid choice. Please select a valid option.");   
 }
     }
 }
