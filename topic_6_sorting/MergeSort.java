5efd388f9cd9a608793817575440e30b84cee737
khaiky <samanthagabitano015@gmail.com>
2024-11-30 14:31:14 +0800

    Initialized first the scanner and the variable for the container of the elements and the the time taken for the MergeSort

---
+++ topic_6_sorting/MergeSort.java
@@ -0,0 +1,24 @@
+
+package topic_6_sorting;
+
+import java.util.Scanner;
+
+public class MergeSort {
+     public static void mergeSort(int[] arr) {
+        if (arr.length < 2) {
+            return;
+        }
+
+        int mid = arr.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[arr.length - mid];
+
+        System.arraycopy(arr, 0, left, 0, mid);
+        System.arraycopy(arr, mid, right, 0, arr.length - mid);
+
+        mergeSort(left);
+        mergeSort(right);
+
+        merge(arr, left, right);
+    }
+}
