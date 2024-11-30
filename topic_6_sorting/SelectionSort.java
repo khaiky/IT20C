c0a78c6b77194a5a40295691fcc5039f323d8713
khaiky <samanthagabitano015@gmail.com>
2024-11-30 15:02:35 +0800

    Added first the SelectionSort and printArray method

---
+++ topic_6_sorting/SelectionSort.java
@@ -0,0 +1,28 @@
+
+package topic_6_sorting;
+
+import java.util.Scanner;
+
+public class SelectionSort {
+    
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
