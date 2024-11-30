5bb2fdecb960cbced9ee93fcb90e89295fb7202d
khaiky <samanthagabitano015@gmail.com>
2024-11-30 14:34:29 +0800

    added while condition and the choice

--- topic_6_sorting/MergeSort.java
+++ topic_6_sorting/MergeSort.java
@@ -21,4 +21,23 @@ public class MergeSort {
 
         merge(arr, left, right);
     }
+     private static void merge(int[] arr, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                arr[k++] = left[i++];
+            } else {
+                arr[k++] = right[j++];
+            }
+        }
+
+        while (i < left.length) {
+            arr[k++] = left[i++];
+        }
+
+        while (j < right.length) {
+            arr[k++] = right[j++];
+        }
+    }
+
 }
