64aa8a363905c35d2304c6cad233adcb156947bd
khaiky <samanthagabitano015@gmail.com>
2024-11-30 13:59:07 +0800

    added array method to print

--- topic_6_sorting/BubbleSort.java
+++ topic_6_sorting/BubbleSort.java
@@ -18,5 +18,13 @@ public class BubbleSort {
                 }
             }
     }
+       public static void printArray(int[] arr) {
+            
+            for (int i : arr) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+
 }
 
