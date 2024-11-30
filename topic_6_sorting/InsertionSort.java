165820ceb4fae3c1fd98d547d3e42cef4a6e8aca
khaiky <samanthagabitano015@gmail.com>
2024-11-30 14:18:14 +0800

    added Case 1 for the Insertion

--- topic_6_sorting/InsertionSort.java
+++ topic_6_sorting/InsertionSort.java
@@ -39,6 +39,19 @@ public class InsertionSort {
                         + " 2. View sorted array and time taken."
                         + " 3. Exit");
                 int choice = sc.nextInt();
+                switch(choice) {
+                    case 1:
+                            System.out.println();
+                            System.out.println("Enter the number of elements you want to store in the array:");
+                            int num = sc.nextInt();
+                            arr = new int[num];
+                        
+                            System.out.println("Enter " + num + " elements.");
+                            for(int i = 0; i < num; i++) {
+                              arr[i] = sc.nextInt();
+                            }
+                            break;
      }
+}
 }
 }
